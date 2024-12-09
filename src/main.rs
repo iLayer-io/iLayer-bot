d9bb0ed4649b8ff988a16d0223a275f471d93025
BePP <gabrio.tognozzi@gmail.com>
2024-12-09 15:09:51 +0100

    alloy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use tokio;
+use tokio::{self, join, task::JoinHandle};
 use eyre::Result;
 
 mod worker;
@@ -6,12 +6,20 @@ mod worker;
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Main function is running...");
-    tokio::spawn(async {
+    let worker_handle: JoinHandle<eyre::Result<()>> = tokio::spawn(async {
         worker::run_worker().await
     });
 
+    let result: (std::result::Result<std::result::Result<(), eyre::Error>, tokio::task::JoinError>,) = join!(worker_handle);
+    match result.0.unwrap() {
+        Ok(_) => {
+            println!("Worker finished successfully");
+        }
+        Err(err) => {
+            eprintln!("Worker failed: {}", err);
+        }
+    }
     // Keep the main thread alive to allow worker to run
-    tokio::signal::ctrl_c().await.unwrap();
     println!("\nExiting...");
     Ok(())
 }
--- src/worker/mod.rs
+++ src/worker/mod.rs
@@ -1,46 +1,32 @@
-use std::sync::Arc;
-
-use ethers::{abi::{Abi, Address}, contract::{Contract, EthEvent}, providers::{Http, StreamExt, Provider}, types::U256};
-use tokio;
+use alloy::{primitives::address, providers::{Provider, ProviderBuilder}, rpc::types::Filter};
 use eyre::Result;
-
-#[derive(Debug, Clone, EthEvent)]
-#[ethevent(name = "OrderCreated", abi = "OrderCreated(uint256,uint256,uint256)")]
-pub struct OrderCreated {
-    pub chain_id: U256, 
-    pub coin_id: U256, 
-    pub amount: U256, 
-}
-
+use futures_util::StreamExt;
 
 pub async fn run_worker()-> Result<()> {
     println!("Worker routine is running...");
-    let provider = Provider::<Http>::try_from("http://127.0.0.1:8545").unwrap();
-    let provider = Arc::new(provider);
-    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
-    let contract_address: Address = "0x82Dc47734901ee7d4f4232f398752cB9Dd5dACcC".parse().unwrap();
-    let abi_bytes  = include_bytes!("../../abi/Counter.json");
-    let abi = Abi::load(&abi_bytes[..]).expect("Failed to parse ABI");
+    let rpc_url = "ws://127.0.0.1:8545";
+    let address = address!("700b6A60ce7EaaEA56F065753d8dcB9653dbAD35");
+    let from_block= 0;
+    let event_name = "OrderCreated(uint256,uint256,uint256)";
 
-    let contract: ethers::contract::ContractInstance<Arc<Provider<Http>>, _> = Contract::new(contract_address, abi, provider.clone());
-    let event = contract.event::<OrderCreated>();
-    
-    // TODO FIXME Filter only events incoming from the contract address
-    let mut event_stream = event.stream().await.unwrap();
+    let provider = ProviderBuilder::new()
+    .on_builtin(rpc_url)
+    .await?;
 
-    while let Some(event_result) = event_stream.next().await {
-        match event_result {
-            Ok(event) => {
-                println!("Received OrderCreated event: {:?}", event);
-            }
-            Err(err) => {
-                eprintln!("Error while receiving event: {:?}", err);
-            }
-        }
+    let filter = Filter::new()
+        .address(address)
+        .event(event_name)
+        .from_block(from_block); // TODO FIXME from block not working
+
+    let sub = provider.subscribe_logs(&filter).await?;
+    let mut stream = sub.into_stream();
+
+    println!("Reading logs...");
+    while let Some(log) = stream.next().await {
+        println!("Processing: {log:?}");
     }
-
-    println!("Listening for OrderCreated events...");
     
+    println!("Worker routine terminated!");
     Ok(())
 
 }
