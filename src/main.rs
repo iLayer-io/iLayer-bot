beb20e44d67ea0d9c1a88da436917eb0005ee84b
BePP <gabrio.tognozzi@gmail.com>
2024-12-10 20:14:11 +0100

    refine main

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,22 @@ mod worker;
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Main function is running...");
-    let worker_handle: JoinHandle<eyre::Result<()>> = tokio::spawn(async {
-        worker::run_worker().await
-    });
 
-    let result: (std::result::Result<std::result::Result<(), eyre::Error>, tokio::task::JoinError>,) = join!(worker_handle);
-    match result.0.unwrap() {
-        Ok(_) => {
-            println!("Worker finished successfully");
-        }
-        Err(err) => {
-            eprintln!("Worker failed: {}", err);
-        }
-    }
-    // Keep the main thread alive to allow worker to run
+    let worker_handle = tokio::spawn(worker::run_subscription_worker());
+    let poll_worker_handle = tokio::spawn(worker::run_poll_worker());
+
+    let (worker_result, poll_worker_result) = join!(worker_handle, poll_worker_handle);
+
+    handle_result("Subscription worker", worker_result);
+    handle_result("Poll worker", poll_worker_result);
+
     println!("\nExiting...");
     Ok(())
+}
+
+fn handle_result(worker_name: &str, result: std::result::Result<eyre::Result<()>, tokio::task::JoinError>) {
+    match result.unwrap() {
+        Ok(_) => println!("{} finished successfully", worker_name),
+        Err(err) => eprintln!("{} failed: {}", worker_name, err),
+    }
 }
--- src/worker/mod.rs
+++ src/worker/mod.rs
@@ -2,7 +2,7 @@ use alloy::{primitives::address, providers::{Provider, ProviderBuilder}, rpc::ty
 use eyre::Result;
 use futures_util::StreamExt;
 
-pub async fn run_worker()-> Result<()> {
+pub async fn run_subscription_worker()-> Result<()> {
     println!("Worker routine is running...");
     let rpc_url = "ws://127.0.0.1:8545";
     let address = address!("700b6A60ce7EaaEA56F065753d8dcB9653dbAD35");
@@ -16,7 +16,7 @@ pub async fn run_worker()-> Result<()> {
     let filter = Filter::new()
         .address(address)
         .event(event_name)
-        .from_block(from_block); // TODO FIXME from block not working
+        .from_block(from_block);
 
     let sub = provider.subscribe_logs(&filter).await?;
     let mut stream = sub.into_stream();
@@ -25,8 +25,34 @@ pub async fn run_worker()-> Result<()> {
     while let Some(log) = stream.next().await {
         println!("Processing: {log:?}");
     }
-    
+
     println!("Worker routine terminated!");
     Ok(())
 
+}
+
+pub async fn run_poll_worker()-> Result<()> {
+    println!("Worker routine is running...");
+    let rpc_url = "http://127.0.0.1:8545";
+    let address = address!("700b6A60ce7EaaEA56F065753d8dcB9653dbAD35");
+    let from_block= 0;
+    let event_name = "OrderCreated(uint256,uint256,uint256)";
+
+    let provider = ProviderBuilder::new()
+    .on_builtin(rpc_url)
+    .await?;
+
+    let filter = Filter::new()
+        .address(address)
+        .event(event_name)
+        .from_block(from_block);
+
+    println!("Reading logs...");
+    let sub = provider.get_logs(&filter).await?;
+    for log in sub {
+        println!("Processing: {log:?}");
+    }
+
+    println!("Worker routine terminated!");
+    Ok(())
 }
