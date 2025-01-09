4508007eb5d6cd58f59bbe680a867ac95fadc039
Matteo Muraca <mmuraca247@gmail.com>
2025-01-09 17:16:03 +0100

    new config.toml

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,15 @@ mod worker;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    dotenv()?;
+    dotenv().ok();
 
     let app_context = context::context()?;
     info!(app_context.logger, "Main function is starting...");
 
-    worker::run_block_listener_poll_worker(&app_context).await?;
-
-    let block_subscription_worker_handle = 
+    let block_subscription_worker_handle =
         worker::run_block_listener_subscription_worker(&app_context);
 
-    let order_filler_worker_handle = 
-        worker::filler::run_order_filler_worker(&app_context);
+    let order_filler_worker_handle = worker::filler::run_order_filler_worker(&app_context);
 
     let result = tokio::select! {
         res = order_filler_worker_handle => ("order_filler_worker", res),
@@ -31,15 +28,18 @@ async fn main() -> Result<()> {
 
     match result {
         (worker_name, Ok(_)) => {
-            warn!(app_context.logger, "{} has terminated unexpectedly", worker_name);
+            warn!(
+                app_context.logger,
+                "{} has terminated unexpectedly", worker_name
+            );
             Ok(())
-        },
+        }
         (worker_name, Err(e)) => {
             error!(
                 app_context.logger,
                 "{} encountered an error: {:?}", worker_name, e
             );
             Err(e.into())
-        },
+        }
     }
 }
--- src/worker/filler.rs
+++ src/worker/filler.rs
@@ -6,10 +6,17 @@ use crate::{context::AppContext, repository::new};
 pub async fn run_order_filler_worker(context: &AppContext) -> Result<()> {
     let order_repository = new(context).await?;
     loop {
-        tokio::time::sleep(tokio::time::Duration::from_millis(context.config.filler_poll_interval)).await;
+        tokio::time::sleep(tokio::time::Duration::from_millis(
+            context.config.filler_poll_interval,
+        ))
+        .await;
         let ready_orders = order_repository.get_ready_orders().await?;
         for order in ready_orders {
-            info!(context.logger, "Trying to fill ready order with order_id: {:?}", hex::encode(order.order_id));
+            info!(
+                context.logger,
+                "Trying to fill ready order with order_id: {:?}",
+                hex::encode(order.order_id)
+            );
             // TODO:
             // 2. Try to Fill the Orders
             //   - call fillOrder on the target smart contract's router
